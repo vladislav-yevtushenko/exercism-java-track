24b0d72b964f50abc639205029abbd49e428fc1d
vladislav-yevtushenko <vladislav.yevtushenko@gmail.com>
2019-03-13 21:39:28 +0600

    Raindrops done

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,20 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
+        String str = "";
+
+        if (number % 3 == 0) {
+            str = str.concat("Pling");
+        } else {
+            str = String.valueOf(number);
+        }
+        if (number % 5 == 0) {
+            str = str.concat("Plang");
+        }
+        if (number % 7 == 0) {
+            str = str.concat("Plong");
+        }
+        return str;
+    }
 }
